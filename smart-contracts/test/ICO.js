c654afc85b3a15e7befa27317bb9b811a4cb6ffb
littleMatch02 <smithginger037@gmail.com>
2023-05-07 07:48:07 -0800

    fix: unused variables removed

--- smart-contracts/test/ICO.js
+++ smart-contracts/test/ICO.js
@@ -2,8 +2,6 @@ const {
     time,
     loadFixture,
 } = require("@nomicfoundation/hardhat-network-helpers");
-const {anyValue} = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const {BigInt} = require("big-integer");
 const {expect} = require("chai");
 const {ethers} = require("hardhat");
 
